7bed7875523d55f35a33528a31b130a13c1bd6df
fgaliz <Freddy.Galiz@gilbarco.com>
2023-09-14 09:01:31 -0300

    esta es la clases de bucles

---
+++ bucles.js
@@ -0,0 +1,12 @@
+suma = 0;
+for (let i = 0; i<5; i++){
+    suma = suma + i;
+    console.log("variable de iteracion:", i);
+}
+console.log("variable suma = ", suma);
+
+suma = 0;
+while(suma<3){
+    suma = suma+1;
+    console.log(suma);
+}
