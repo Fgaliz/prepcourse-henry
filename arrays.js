70d3080dc79904a08bb7b0984d4d46e277cd9c17
fgaliz <Freddy.Galiz@gilbarco.com>
2023-09-20 16:20:59 -0300

    esta es la clase de arrays

---
+++ arrays.js
@@ -0,0 +1,11 @@
+// list de compras
+var listadeCompras = []
+listadeCompras [3] = "lechuga";
+listadeCompras [1] = "tomates";
+console.log(listadeCompras)
+// ver lista de compras
+var elementosDelArray = listadeCompras[1];
+console.log(elementosDelArray);
+//lenght
+var nombres = ['Matias', 'Maria', 'Diego', 'Rosa'].length;
+console.log(nombres);
