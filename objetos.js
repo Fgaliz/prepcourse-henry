a5382293768f0e19a75c68f8c588bf90f5031c7b
fgaliz <Freddy.Galiz@gilbarco.com>
2023-09-28 17:14:37 -0300

    clase de objetos modulo 6

---
+++ objetos.js
@@ -0,0 +1,9 @@
+var deportes = {
+    conBalon: ['Futbol', 'Basketball', 'Golf'],
+    sinBalon: ['Boxeo', 'Surf', 'Trekking'],
+};
+deportes.conBalon.pop()
+console.log(deportes.conBalon);
+
+var persona = {nombre: 'Lucas', edad: 26, estudios: {esProgramador: true}};
+console.log(persona.nombre)
