668980f912a11c953152c88afba43304fee93dcd
fgaliz <Freddy.Galiz@gilbarco.com>
2023-09-28 17:35:45 -0300

    update de las clases m06 objetos

--- objetos.js
+++ objetos.js
@@ -7,3 +7,23 @@ console.log(deportes.conBalon);
 
 var persona = {nombre: 'Lucas', edad: 26, estudios: {esProgramador: true}};
 console.log(persona.nombre)
+
+//Acceder
+var persona = { nombre: 'Lucas', edad: 26, estudios: {esProgramador:true}};
+console.log(persona.edad);
+
+//ASIGNAR
+var persona = { nombre: 'Lucas', edad: 26, estudios: {esProgramador:true}};
+persona.nombre = "Martin";
+console.log(persona.nombre);
+
+//CREAR
+var autos = {};
+autos.marcas = ['Ford', 'Audi', 'Ferrari'];
+console.log(autos);
+autos.marcas.push('Fiat');
+console.log(autos.marcas);
+
+//BORRAR
+delete autos.marcas;
+console.log(autos);
