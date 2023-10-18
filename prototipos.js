b8b24db6c8c773d2513d97e2a71badf7c2f110cd
fgaliz <Freddy.Galiz@gilbarco.com>
2023-10-18 20:59:10 -0300

    prototipos actualizado

---
+++ prototipos.js
@@ -0,0 +1,43 @@
+//Devuelve un array con los valores mayores a 3 en false:
+
+Array.prototype.mayorQueTres = function () {
+    var arregloModificado = [];
+    for (var i = 0; i < this.length; i++) {
+           if (this[ i ] > 3) {
+                  arregloModificado.push(false);
+           } else {
+                  arregloModificado.push(this[ i ]);
+           }
+    }
+    return arregloModificado;
+ };
+  
+ var arreglo = [1, 2, 3, 4, 5];
+ var nuevoArreglo = arreglo.mayorQueTres();
+ console.log(nuevoArreglo);
+ class LatinoAmerica {
+	constructor() {
+		this.paises = [ ];
+	};
+};
+ //Agrega Pais
+
+LatinoAmerica.prototype.agregarPais = function (pais) {
+   	this.paises.push(pais);
+};
+let continente = new LatinoAmerica();
+continente.agregarPais('México');
+console.log(continente.paises);
+
+// Agrega peliculas
+class Peliculas {
+       constructor(){
+         this.pelis = [];
+       }
+       }
+       Peliculas.prototype.agregarPeli = function(pelicula){
+         this.pelis.push(pelicula);
+       }
+       var peliculasFavoritas = new Peliculas();
+       peliculasFavoritas.agregarPeli(["rocky 1", "Aliens", "naranja mecanica"]);
+       console.log("mis peliculas favoritas son "+peliculasFavoritas.pelis);
